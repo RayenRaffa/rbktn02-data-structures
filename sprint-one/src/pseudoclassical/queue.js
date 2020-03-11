4f6db6b4dcfa827719db920f672b72efe8d618ab
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-11 20:13:23 +0100

    add pseudoclassical stack & queue implementation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
 };
 
 
+Queue.prototype.enqueue = function(value) {
+	let firstIndex = Object.keys(this.storage)[0]; 
+	this.storage[firstIndex+this.size()] = value;
+};
+Queue.prototype.dequeue = function() {
+    let element =this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    return element;
+};
+
+Queue.prototype.size = function() {
+
+	return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
 };
 
 
+Stack.prototype.push = function(value) {
+	this.storage[this.size()] = value;
+};
+Stack.prototype.pop = function() {
+	let top = this.size() - 1;
+	let element = this.storage[top];
+	delete this.storage[top];
+	return element;
+};
+Stack.prototype.size = function() {
+	return Object.keys(this.storage).length;
+};
