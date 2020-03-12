e7da727b9eb016169f7cd4df9ad27415ef69639a
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-12 17:59:51 +0100

    add linkedList implementation

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let node = new Node(value); 
+    if(!this.tail && !this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+
   };
 
   list.removeHead = function() {
+    let head = this.head;
+    this.head = this.head.next;
+    return head.value;
   };
 
   list.contains = function(target) {
+    let node = this.head;
+    while(node) {
+      if(node.value == target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   };
 
   return list;
