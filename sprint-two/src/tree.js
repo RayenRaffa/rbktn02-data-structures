35cbf3fb7b7250b755f114320b718c64de5f240f
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-13 11:25:03 +0100

    add tree implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,38 @@
 var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
+	var newTree = {};
+	newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value1) {
+	this.children.push(Tree(value1));
 };
 
 treeMethods.contains = function(target) {
+	let node = this;
+	while(node) {
+		if (node.value == target) {
+			return true;
+		} else {
+			if (node.children.length) {
+				for (var i=0; i<this.children.length; i++) {
+					node = this.children[i];
+					if (node.contains(target)) {
+						return true;
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+	return false;
 };
 
 
