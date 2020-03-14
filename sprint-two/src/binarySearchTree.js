a2115e424e8ef6cbff4e193dfc3b6d273073f878
Rayen RAFFA <raffa.rayen@gmail.com>
2020-03-14 15:12:33 +0100

    add binary  search tree implementation

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,78 @@
 var BinarySearchTree = function(value) {
+	var newBST = {};
+	newBST.value = value;
+
+  // your code here
+  newBST.left =null;  // fix me
+  newBST.right =null;  // fix me
+
+  _.extend(newBST, BSTmethods);
+  return newBST;
 };
+var BSTmethods={};
+
+BSTmethods.insert=function(value){
+	//let node=this
+	let newNode=BinarySearchTree(value)
+	if (value>this.value) {
+		if (this.right){
+			this.right.insert(value);
+		} else { 
+			this.right=newNode;
+		}
+	}
+	else if (value<this.value) {
+		if (this.left){
+			this.left.insert(value);
+		} else {
+			this.left=newNode;
+		}
+	}
+
+}
+
+BSTmethods.contains=function(target){
+	
+	if (this.value===target) {
+		return true; 
+	}else if (this.value<target) {
+		return !!this.right &&(this.right.contains(target));
+		// if (this.right) {				
+		// 	return this.right.contains(target);
+		// } else {
+		// 	return false;
+		// }
+	} else {
+		// if (this.left) {
+		// 	return this.left.contains(target);
+		// } else {
+		// 	return false;
+		// }
+		return !!this.left && (this.left.contains(target));
+	}
+}
+
+BSTmethods.depthFirstLog=function(callback){
+	let node = this;
+	while(node) {
+		callback(node.value)
+		if (node.left) {
+			node = this.left; 
+
+			node.depthFirstLog(callback)	
+		} 
+		if (node.right) {
+			node = this.right; 
+			node.depthFirstLog(callback);
+		}
+		if (!node.left && !node.right) {
+			break;
+		} 
+	}
+};
+
+
+
 
 
 /*
